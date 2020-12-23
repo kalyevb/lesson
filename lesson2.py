7f4bfe32b3edabb91066f8d329490014b5edaa36
kalyevb <kayevbb@mail.com>
2020-12-23 15:52:18 +0600

    a

---
+++ lesson2.py
@@ -0,0 +1,20 @@
+# # data = int(input("Введите значение"))
+# # asd = input("asdasd")
+# # qq = int(input('asd'))
+# #
+# # if type(data) == type(int):
+# #     print ("Aaa")
+# # else:
+# #     print ('asdas')
+# # #
+# # # if data == 10:
+# # #     print(data)
+#
+# a = [1, 1, 2, 1]
+# b = {4, 4, 4, 4}
+#
+
+
+
+
+
